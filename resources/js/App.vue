37ab7e6ae8782fae437a39c543ad037647b0cd89
Sergio Lopez <dev.master@chrisarcadia.com>
2026-02-16 18:34:55 -0700

    SL - Instalacion de dependencias y Vue Js

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>AgendaVB 🚀</h1>
+        <p>Vue 3 funcionando correctamente</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
