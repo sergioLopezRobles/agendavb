778254603aa90ccdeb624025c99d505b0048f7b1
AlmaMR <alanmercadora@ittepic.edu.mx>
2026-04-21 14:15:42 -0700

    AAMR - Ruta actualizar estado cita

--- routes/api.php
+++ routes/api.php
@@ -65,6 +65,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     // Citas Negocio
     Route::get('/citas-negocios', [CitasNegociosController::class, 'citasnegocios']);
     Route::get('/citas-negocios/{id_negocio}', [CitasNegociosController::class, 'obtenerCitasNegocio']);
+    Route::post('/actualizar-estado-cita', [CitasNegociosController::class, 'actualizarEstadoCita']);
+
     //CREAR NUEVO NEGOCIO PLANES MEDIO-AVANZADO
     Route::post('/negocios-extra', [NegocioController::class, 'crearNegocioExtra']);
 });
